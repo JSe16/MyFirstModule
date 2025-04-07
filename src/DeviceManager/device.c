f8e642fa21784713a3d67270665870462d12c6b6
JSe16 <Johannes.Sellmaier@Qwicklane.com>
2025-04-07 14:59:12 +0200

    d
    d

--- src/DeviceManager/device.c
+++ src/DeviceManager/device.c
@@ -4,17 +4,15 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include "myFirstTry.h"
+#include "device.h"
  #include <zephyr/kernel.h>
  #include <zephyr/logging/log.h>
 /* Stackgröße und Priorität für den Thread */
 
 
-LOG_MODULE_REGISTER(device, CONFIG_APP_LOG_LEVEL);
 
 void init_device(device_t *dev, uint32_t id, uint32_t flags)
 {
 	dev->id = id;
 	dev->flags = flags;
-	LOG_INF("Device initialized with ID: %d and flags: %d", id, flags);
 }
